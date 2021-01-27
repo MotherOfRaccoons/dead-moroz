fcbbb054cb40a513841f4c8a77b842ef9d463975
motherofraccoons <mr.bronium@gmail.com>
2021-01-27 18:32:38 +0300

    Move FontAwesome import to application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,12 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
 import "jquery"
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+library.add(fas)
+dom.watch()
