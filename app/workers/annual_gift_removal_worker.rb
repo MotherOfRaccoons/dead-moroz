d6a05952f72248fff3dcfd72740cbfcf4c0590db
motherofraccoons <mr.bronium@gmail.com>
2021-02-09 16:24:05 +0300

    Change congratulation_email arguments

--- app/workers/annual_gift_removal_worker.rb
+++ app/workers/annual_gift_removal_worker.rb
@@ -2,8 +2,8 @@ require 'sidekiq-scheduler'
 
 class AnnualGiftRemovalWorker < ApplicationWorker
   def perform
-    User.elf.with_selected_gifts.pluck(:email, :first_name, :gender).each do |email, first_name, gender|
-      CongratulationMailer.congratulation_email(email, first_name, gender).deliver_later
+    User.elf.with_selected_gifts.each do |user|
+      CongratulationMailer.congratulation_email(user).deliver_later
     end
     Gift.where(selected: true).destroy_all
   end
